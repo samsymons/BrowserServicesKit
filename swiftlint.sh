62c67f1ad66116e8ba1c1170437bb0f184e2d9b8
Sam Symons <sam@samsymons.com>
2021-08-24 21:08:37 -0700

    Pass github.base_ref as an environment variable.

--- swiftlint.sh
+++ swiftlint.sh
@@ -17,7 +17,7 @@ then
 	cd ${WORKING_DIRECTORY}
 fi
 
-changedFiles=$(git --no-pager diff --name-only --relative FETCH_HEAD $(git merge-base FETCH_HEAD github.base_ref) -- '*.swift')
+changedFiles=$(git --no-pager diff --name-only --relative FETCH_HEAD $(git merge-base FETCH_HEAD $BASE_BRANCH) -- '*.swift')
 
 if [ -z "$changedFiles" ]
 then
